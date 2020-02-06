1b0af4c149018e5d837fabf8ea2ab3596e95d79f
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-02-06 10:57:05 +0100

    opdracht 5 klaar

--- opdracht05/js/script.js
+++ opdracht05/js/script.js
@@ -24,20 +24,24 @@ function toggleRoodLicht(){
 }
 
 function toggleOranjeLicht(){
-    if(document.getElementById("light-middle").style.backgroundColor == "gray"){
+    if(oranjeLichtStaatAan == false){
         document.getElementById("light-middle").style.backgroundColor = "orange"
+        oranjeLichtStaatAan = true;
     }
     else{
         document.getElementById("light-middle").style.backgroundColor = "gray";
+        orangeLichtStaatAan = false;
     }
 }
 
 function toggleGroenLicht(){
-    if(document.getElementById("light-bottom").style.backgroundColor == "gray"){
+    if(groenLichtStaatAan == false){
         document.getElementById("light-bottom").style.backgroundColor = "green";
+        groenLichtStaatAan = true
     }
     else{
         document.getElementById("light-bottom").style.backgroundColor = "gray";
+        groenLichtStaatAan = false;
     }
     
 }
