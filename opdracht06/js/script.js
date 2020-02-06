13b45578cab80237a41a7326db3c53a1b11cb72f
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-02-06 11:02:54 +0100

    opdracht 6 klaar

--- opdracht06/js/script.js
+++ opdracht06/js/script.js
@@ -7,46 +7,46 @@ var oranjeLichtStaatAan = false;
 var groenLichtStaatAan = false;
 
 window.onload = function(){
-    document.getElementById("left-light-top").innerHTML = stop;
-    document.getElementById("left-light-middle").innerHTML = maybe;
-    document.getElementById("left-light-bottom").innerHTML = go;
+    document.getElementById("a-light-top").innerHTML = stop;
+    document.getElementById("a-light-middle").innerHTML = maybe;
+    document.getElementById("a-light-bottom").innerHTML = go;
 
-    document.getElementById("right-light-top").innerHTML = stop;
-    document.getElementById("right-light-middle").innerHTML = maybe;
-    document.getElementById("right-light-bottom").innerHTML = go;
+    document.getElementById("b-light-top").innerHTML = stop;
+    document.getElementById("b-light-middle").innerHTML = maybe;
+    document.getElementById("b-light-bottom").innerHTML = go;
 
     //JOUW CODE HIER
 }
 
 function toggleRoodLicht(){
     if(roodLichtStaatAan == false){
-        document.getElementById("left-light-top").style.backgroundColor = "red";
+        document.getElementById("a-light-top").style.backgroundColor = "red";
         roodLichtStaatAan = true;
     }
     else{
-        document.getElementById("left-light-top").style.backgroundColor = "gray";
+        document.getElementById("a-light-top").style.backgroundColor = "gray";
         roodLichtStaatAan = false;
     }
 }
 
 function toggleOranjeLicht(){
     if(oranjeLichtStaatAan == false){
-        document.getElementById("left-light-middle").style.backgroundColor = "orange";
+        document.getElementById("a-light-middle").style.backgroundColor = "orange";
         oranjeLichtStaatAan = true;
     }
     else{
-        document.getElementById("left-light-middle").style.backgroundColor = "gray";
+        document.getElementById("a-light-middle").style.backgroundColor = "gray";
         oranjeLichtStaatAan = false;
     }
 }
 
 function toggleGroenLicht(){
     if(groenLichtStaatAan == false){
-        document.getElementById("left-light-bottom").style.backgroundColor = "green";
+        document.getElementById("a-light-bottom").style.backgroundColor = "green";
         groenLichtStaatAan = true;
     }
     else{
-        document.getElementById("left-light-bottom").style.backgroundColor = "gray";
+        document.getElementById("a-light-bottom").style.backgroundColor = "gray";
         groenLichtStaatAan = false;
     }
 }
