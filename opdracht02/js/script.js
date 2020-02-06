647a8d1ed0618813cb5277827b0bb9747309b3fc
donnevspankeren <donne.van.spankeren01@gmail.com>
2020-02-06 10:39:00 +0100

    opdracht 2 klaar

--- opdracht02/js/script.js
+++ opdracht02/js/script.js
@@ -3,9 +3,9 @@ var maybe = "oranje";
 var go = "groen";
 
 window.onload = function(){
-    document.getElementById("light-top").innerHTML = stop;
-    document.getElementById("light-middle").innerHTML = maybe;
-    document.getElementById("light-bottom").innerHTML = go;
+    document.getElementById("light-top").style.backgroundColor = "red";
+    document.getElementById("light-middle").style.backgroundColor = "orange";
+    document.getElementById("light-bottom").style.backgroundColor = "green";
 
     //JOUW CODE HIER
 }
